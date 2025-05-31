a7813e3b3383bf8ed7e0da8caccd5bbdc39d408e
Simon Olander Sahlén <simon.olander@gmail.com>
2025-05-31 19:56:21 +0200

    Add MUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import type { Metadata } from "next";
 import "./globals.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 export const metadata: Metadata = {
   title: "Jorden",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+'use client'
+
 import countries from "./countries.json"
+import {Box, Button, Stack, Typography, useMediaQuery} from "@mui/material";
 
 export default function Home() {
+    const landscape = useMediaQuery('(orientation: landscape)')
   const country = countries[Math.floor(Math.random() * countries.length)];
   const options = [country]
   while (options.length < 4) {
@@ -13,22 +16,25 @@ export default function Home() {
   }
   options.sort(() => Math.random() - 0.5)
   return (
-      <div>
-        <h1>Where are we?</h1>
-          <Image
-              src={country.svg}
-              alt={"Where are we?"}
-              width={200}
-              height={200}
-          />
-          {
-            options.map((option, i) => (
-                <button key={i}
-                 >
-                  {option.country}
-                </button>
-            ))
-          }
-      </div>
+      <Stack direction={landscape ? "row" : "column"} sx={{p: 2}} spacing={2} justifyContent="center" alignItems="center">
+          <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
+              <Typography variant="h3">Where are we?</Typography>
+              <Box
+                  component="img"
+                  src={country.svg}
+                  alt={"Where are we?"}
+                  width="100%"
+              />
+          </Stack>
+          <Stack direction="column" spacing={2} justifyContent="center" fullfillWidth>
+              {
+                  options.map((option, i) => (
+                      <Button key={i} variant="contained" fullWidth>
+                          {option.country}
+                      </Button>
+                  ))
+              }
+          </Stack>
+      </Stack>
   )
 }
