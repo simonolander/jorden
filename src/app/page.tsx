f6ecd98c57392d88555389a82355b01510b4197d
Simon Olander Sahlén <simon.olander@gmail.com>
2025-05-31 19:03:30 +0200

    Remove tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,28 +6,29 @@ export default function Home() {
   const options = [country]
   while (options.length < 4) {
     let option = countries[Math.floor(Math.random() * countries.length)];
-    if (option == country) {
+    if (options.some(opt => opt.country === option.country)) {
       continue
     }
     options.push(option)
   }
+  options.sort(() => Math.random() - 0.5)
   return (
-      <div
-          className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <h1 className="text-4xl font-bold text-center sm:text-left">Where is this?</h1>
+      <div>
+        <h1>Where are we?</h1>
           <Image
               src={country.svg}
-              alt={"The country to guess"}
+              alt={"Where are we?"}
               width={200}
               height={200}
           />
-          <ul className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-            {options.map((option, i) => (
-                <li key={i} className="mb-2 tracking-[-.01em]">{option.country}</li>
-            ))}
-          </ul>
-        </main>
+          {
+            options.map((option, i) => (
+                <button key={i}
+                 >
+                  {option.country}
+                </button>
+            ))
+          }
       </div>
   )
 }
