24a98b1629305159ebc71b1b95f1c545f3afae84
Simon Olander Sahlén <simon.olander@gmail.com>
2025-06-01 15:02:05 +0200

    Add game logic and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,103 @@
-import Image from "next/image";
+'use client'
+
+import countries from "./countries.json"
+import {useState} from "react";
+
+interface Country {
+    country: string,
+    svg: string,
+}
+
+interface GameState {
+    country: Country,
+    options: Country[],
+    answer: Country | undefined
+}
+
+function newState(): GameState {
+    const country = countries[Math.floor(Math.random() * countries.length)]
+    const options = [country]
+    const candidates = countries.filter(it => it.tags.some(tag => country.tags.includes(tag)))
+    while (candidates.length < 10) {
+        const candidate = countries[Math.floor(Math.random() * countries.length)]
+        if (candidates.some(opt => opt.country === candidate.country)) {
+            continue
+        }
+        candidates.push(candidate)
+    }
+    while (options.length < 4) {
+        let option = candidates[Math.floor(Math.random() * candidates.length)];
+        if (options.some(opt => opt.country === option.country)) {
+            continue
+        }
+        options.push(option)
+    }
+    options.sort(() => Math.random() - 0.5)
+    return {
+        country,
+        options,
+        answer: undefined
+    }
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const [state, setState] = useState(newState)
+    const {country, options, answer} = state
+    const [score, setScore] = useState(0)
+    const [total, setTotal] = useState(0)
+    const [loading, setLoading] = useState(true)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    const submitAnswer = (answer: Country) => {
+        setState(it => ({...it, answer}))
+        setTotal(it => it + 1)
+        if (answer === country) {
+            setScore(it => it + 1)
+        }
+    }
+
+    const onNewGameClick = function () {
+        setState(newState)
+        setLoading(true)
+    }
+
+    return (
+        <div className="root">
+            <div className="container">
+                <img
+                    className={`map ${loading ? 'loading' : ''}`}
+                    onLoad={() => setLoading(false)}
+                    src={country.svg}
+                    alt="A map with a region highlighted in green"
+                />
+                <div className="options">
+                    {answer
+                        ? (
+                            <>
+                                {
+                                    answer === country
+                                        ? (
+                                            <p className="success-message">
+                                                <span className="correct-country-name">{country.country}</span>, nice!
+                                            </p>
+                                        )
+                                        : (
+                                            <p className="error-message">
+                                                Not {answer.country}, but <span className="correct-country-name">{country.country}</span>!
+                                            </p>
+                                        )
+                                }
+                                <button className="new-game" onClick={() => onNewGameClick()}>Again!</button>
+                                <p className="score">You've answered <strong>{score} out of {total}</strong> correctly!</p>
+                            </>
+                        )
+                        : (
+                            options.map((option, i) => (
+                                <button key={i} onClick={() => submitAnswer(option)}>{option.country}</button>
+                            ))
+                        )
+                    }
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  )
 }
